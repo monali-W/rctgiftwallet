fa628bb5d1ac30a42dbce84f74645f060f4b2973
Monali-W <wasekarmonaligmail.com>
2022-01-29 19:08:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 
-import Directory from "./components/DirectoryComponent";
-import Menubar from "./components/NavbarComponent";
+import Main from "./components/MainComponent";
+
 import "./App.css";
-import { GIFTCARDS } from "./shared/GiftCards";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      giftcards: GIFTCARDS
-    };
-  }
   render() {
     return (
       <div className="App">
-        <Menubar />
-        <Directory giftcards={this.state.giftcards} />
+        <Main />
       </div>
     );
   }
